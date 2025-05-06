39a27362f30f55c8de4e3872ccf727a8984021ef
Lucas Javier <lucasjavierf10@gmail.com>
2025-05-06 20:15:29 -0300

    Primer commit de mi portfolio con tailwind

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import './style.css'
+/* --------------------------------------menu desplegable------------------------------------ */
+const btnMenu = document.querySelector('.burger')
+const menuDesplegable = document.querySelector('.side-bar')
+
+btnMenu.addEventListener('click', () => {
+    menuDesplegable.classList.toggle('ocultar') 
+})
+
+
+/* --------------------------------------------modo oscuro----------------------------------- */
+const  btnswitch=document.querySelector('#switch')
+const btnswitch2=document.querySelector('#switch2')
+function toggleDarkMode() {
+    document.body.classList.toggle('dark');
+  }
+btnswitch.addEventListener('click',toggleDarkMode)
+btnswitch2.addEventListener('click',toggleDarkMode)
