e4dabb3519550d56acd253ae23c1ea773f9cf134
unknown <lucasjavierf10@gmail.com>
2025-08-31 14:49:59 -0300

    Agregue Slider para los certificados utilizando la libreria  SwipperJS

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import './style.css'
+
+
+
 /* --------------------------------------menu desplegable------------------------------------ */
 const btnMenu = document.querySelector('.burger')
 const menuDesplegable = document.querySelector('.side-bar')
@@ -27,3 +30,37 @@ document.getElementById('contenedor-slide').appendChild(iconos);
 /* AOS.init();
  */
 
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+
+  //responsive breakpoints
+  breakpoints: {
+    0:{
+      slidesPerView: 1
+    },
+    620:{
+      slidesPerView: 2
+    },
+    1024:{
+      slidesPerView: 3
+    }
+  }
+});
